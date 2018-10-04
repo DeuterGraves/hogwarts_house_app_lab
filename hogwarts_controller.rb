bcb1380f4f680d4132d9e931a8630d4cb4bccb87
DeuterGraves <puckles@gmail.com>
2018-10-04 13:27:14 +0100

    hogwarts_constroller

---
+++ hogwarts_controller.rb
@@ -0,0 +1,12 @@
+require("sinatra")
+require("sinatra/contrib/all")
+require("pry")
+require_relative("./models/student.rb")
+also_reload("./models/*")
+
+
+# INDES
+get ('/hogwarts') do
+  @students = Student.all()
+  erb(:index)
+end
